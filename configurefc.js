c4c0a1cdfaa2717bf3ac0d9436b97e9a1022e392
kryptoefx <kryptoefx@gmail.com>
2026-03-21 19:03:43 +0530

    Update and rename style.css to configurefc.js

---
+++ configurefc.js
@@ -0,0 +1,11 @@
+const DOMAIN_CONFIG = {
+    allowedDomains: [
+        "famcodexweb.pages.dev",
+        "famcodexwebs.pages.dev"
+        "kryptoefx.github.io"
+    ]
+};
+
+const TELEGRAM_CONFIG = {
+    popupChannel: "https://telegram.me/+L7agYSmJVRNkZDFl"
+};
