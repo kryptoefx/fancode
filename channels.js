01984534470e22c25959bb8ca6ddd081152131c6
kryptoefx <kryptoefx@gmail.com>
2026-04-12 14:14:49 +0530

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,34 +1,11 @@
-{
-  "domainConfig": {
-    "allowedDomains": [
-      "b4xsports.gamesaved.xyz",
-      "https://b4xsports.gamesaved.xyz",
-      "www.b4xsports.gamesaved.xyz/",
-      "kryptoefx.github.io"
-    ],
-    "popupChannel": "https://www.b4xsports.in"
-  },
-
-  "channels": [
-    {
-      "id": "mncsport1",
-      "manifestUri": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/dr_spotv2hd/default/index.mpd",
-      "drmConfig": {
-        "clearKeys": {
-          "7eea72d6075245a99ee3255603d58853": "6848ef60575579bf4d415db1032153ed"
-        }
-      }
-    },
-    {
-      "id": "USA1",
-      "manifestUri": "https://live-oneapp-prd-news.akamaized.net/Content/CMAF_OL2-CTR-4s-v2/Live/channel(kvea)/master.mpd",
-      "drmConfig": {
-        "clearKeys": {
-          "ce7ab3022e753307997f58afe001bac4": "72d631a66e635c60829a0fe7705516c1"
-        }
+const videoConfigs = [
+  {
+    id: "mncsport1",
+    manifestUri: "STREAM_URL",
+    drmConfig: {
+      clearKeys: {
+        "key": "value"
       }
     }
-  ]
-}
-
-
+  }
+];
