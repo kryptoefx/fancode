62cfaddbf78ba0c870e4f2cfb18473f466d695e1
kryptoefx <kryptoefx@gmail.com>
2026-04-12 14:18:17 +0530

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,11 +1,43 @@
 const videoConfigs = [
+
   {
-    id: "mncsport1",
-    manifestUri: "STREAM_URL",
+    id: 'mncsport1',
+    manifestUri: 'https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/dr_spotv2hd/default/index.mpd',
     drmConfig: {
       clearKeys: {
-        "key": "value"
+        '7eea72d6075245a99ee3255603d58853': '6848ef60575579bf4d415db1032153ed'
+      }
+    }
+  },
+
+  {
+    id: 'USA1',
+    manifestUri: 'https://live-oneapp-prd-news.akamaized.net/Content/CMAF_OL2-CTR-4s-v2/Live/channel(kvea)/master.mpd',
+    drmConfig: {
+      clearKeys: {
+        'ce7ab3022e753307997f58afe001bac4': '72d631a66e635c60829a0fe7705516c1'
+      }
+    }
+  },
+
+  {
+    id: 'USA2',
+    manifestUri: 'https://live-oneapp-prd-news.akamaized.net/Content/CMAF_OL2-CTR-4s/Live/channel(universo-east)/master.mpd',
+    drmConfig: {
+      clearKeys: {
+        '96b67fa09140329a89b7a1ead134eda3': 'c618d32c56cdc6cad8330eb98507ca3f'
+      }
+    }
+  },
+
+  {
+    id: 'fs1',
+    manifestUri: 'https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/fsu3wl2rwb/out/v1/be61206c04e643349d53fb22301f3b50/cenc.mpd',
+    drmConfig: {
+      clearKeys: {
+        '5466ebd70704bdeb657f0abf3c5ca4ef': 'bdd79b72d8e48ed483aa623cc38a8a16'
       }
     }
   }
+
 ];
